de820d6bb16e675c29909717abfe32c0368aab42
Mark Sagi-Kazar <mark.sagikazar@gmail.com>
2021-05-09 08:11:57 +0200

    feat: add initial version
    Signed-off-by: Mark Sagi-Kazar <mark.sagikazar@gmail.com>

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/sagikazarmark/gpassword-prompt
 
 go 1.16
+
+require (
+	fyne.io/fyne/v2 v2.0.3
+	github.com/spf13/pflag v1.0.5
+)
--- main.go
+++ main.go
@@ -1,5 +1,119 @@
 package main
 
-func main() {
+import (
+	_ "embed"
+	"fmt"
+	"log"
+	"os"
 
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/widget"
+	"github.com/spf13/pflag"
+)
+
+//go:embed icon.png
+// nolint: gochecknoglobals
+var icon []byte
+
+func main() {
+	windowTitle := pflag.String("title", "Enter your password", "Window title for the password prompt")
+
+	pflag.Parse()
+
+	app := app.New()
+
+	icon := &fyne.StaticResource{
+		StaticName:    "icon.png",
+		StaticContent: icon,
+	}
+	app.SetIcon(icon)
+
+	window := app.NewWindow(truncateString(*windowTitle, 25)) // TODO: arbitrary truncation
+	window.SetIcon(icon)
+	window.CenterOnScreen()
+	window.SetPadded(true)
+	window.SetFixedSize(true)
+	window.Resize(fyne.NewSize(300, 50))
+
+	var cancelled bool
+	onCancel := func() {
+		log.Println("Cancelled password input")
+
+		cancelled = true
+		window.Close()
+	}
+
+	window.SetCloseIntercept(onCancel)
+
+	entry := newCancelledEntry(onCancel)
+	entry.SetPlaceHolder("Enter your password...")
+	entry.Password = true
+	entry.Wrapping = fyne.TextTruncate
+	entry.OnSubmitted = func(s string) {
+		fmt.Print(s)
+
+		window.Close()
+	}
+	entry.OnCancel = onCancel
+
+	window.Canvas().SetOnTypedKey(func(key *fyne.KeyEvent) {
+		switch key.Name {
+		case fyne.KeyEscape:
+			onCancel()
+		default:
+			// set focus to the entry
+			window.Canvas().Focus(entry)
+			entry.TypedKey(key)
+		}
+	})
+
+	container := container.NewVBox(entry)
+	window.SetContent(container)
+
+	window.Canvas().Focus(entry)
+
+	window.Show()
+
+	app.Run()
+
+	if cancelled {
+		os.Exit(1)
+	}
+}
+
+type cancelledEntry struct {
+	widget.Entry
+
+	OnCancel func()
+}
+
+func newCancelledEntry(onCancel func()) *cancelledEntry {
+	entry := &cancelledEntry{}
+
+	entry.ExtendBaseWidget(entry)
+
+	return entry
+}
+
+func (e *cancelledEntry) TypedKey(key *fyne.KeyEvent) {
+	switch key.Name {
+	case fyne.KeyEscape:
+		if e.OnCancel != nil {
+			e.OnCancel()
+		} else {
+			e.Entry.TypedKey(key)
+		}
+	default:
+		e.Entry.TypedKey(key)
+	}
+}
+
+func truncateString(s string, l int) string {
+	if len(s) < l {
+		return s
+	}
+
+	return s[:l]
 }
